a2e326010f677f2079661b9a70e965b4fb2a8716
vano <kolesen.ivan@gmail.com>
2018-11-06 11:28:01 +0200

    feat: add config with routes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Popup from "./Popup";
 import Header from "./Header";
 import Main from "./Main";
+import { routes } from "./config";
 
 class App extends Component {
+  state = {
+    input: "",
+    routes: routes
+  };
+
   render() {
     return (
       <Fragment>
